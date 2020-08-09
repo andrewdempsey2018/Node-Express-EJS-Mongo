0ee9291d7c4e6b28c37eb827c6bb360ce6d07d83
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-08-09 13:39:37 +0100

    added index view that uses database data

--- app.js
+++ app.js
@@ -36,65 +36,77 @@ app.listen(port);
 app.use(express.static('public'));
 
 //routes, admin add content
-app.get('/add-blog', (req, res) => {
+//app.get('/add-blog', (req, res) => {
 
-    const blog = new Blog({
-        title: 'girly firly',
-        snippet: 'slippedy sloppedy',
-        body: '1 swig 2 flig 3 crabo'
-    });
+//    const blog = new Blog({
+//        title: 'girly firly',
+//        snippet: 'slippedy sloppedy',
+//        body: '1 swig 2 flig 3 crabo'
+//    });
 
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+//    blog.save()
+//        .then((result) => {
+//            res.send(result);
+//        })
+//        .catch((err) => {
+//            console.log(err);
+//        })
+//});
 
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+//app.get('/all-blogs', (req, res) => {
+//    Blog.find()
+//        .then((result) => {
+//            res.send(result);
+//        })
+//        .catch((err) => {
+//            console.log(err);
+//        })
+//});
 
-app.get('/single-blog', (req, res) => {
-    Blog.findById('5f2fca5bb64c76034038789f')
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+//app.get('/single-blog', (req, res) => {
+//    Blog.findById('5f2fca5bb64c76034038789f')
+//        .then((result) => {
+//            res.send(result);
+//        })
+//        .catch((err) => {
+//            console.log(err);
+//        })
+//});
 
 //routes, user navigate website
 
 app.get('/', (req, res) => {
 
-    const blogs = [{
-            title: 'Blog1 title',
-            snippet: 'blog1 snippet snippet1 snippet etc1'
-        },
-        {
-            title: 'Blog2 title',
-            snippet: 'blog2 snippet snippet2 snippet etc2'
-        },
-        {
-            title: 'Blog3 title',
-            snippet: 'blog3 snippet snippet3 snippet etc3'
-        },
-    ];
+    //const blogs = [{
+    //        title: 'Blog1 title',
+    //        snippet: 'blog1 snippet snippet1 snippet etc1'
+    //    },
+    //    {
+    //        title: 'Blog2 title',
+    //        snippet: 'blog2 snippet snippet2 snippet etc2'
+    //    },
+    //    {
+    //        title: 'Blog3 title',
+    //        snippet: 'blog3 snippet snippet3 snippet etc3'
+    //    },
+    //];
 
-    res.render('index', {
-        title: 'hello',
-        blogs
-    });
+    //res.render('index', {
+    //    title: 'hello',
+    //    blogs
+    //});
+
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+        .then((result) => {
+            res.render('index', {blogs: result});
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 app.get('/about', (req, res) => {
